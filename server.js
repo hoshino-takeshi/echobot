ff51414d137596a218e1f24ec8f391bbed754232
hoshino-takeshi <hoshino-takeshi@mki.co.jp>
2016-06-01 15:15:30 +0900

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,20 @@ var botConnectorOptions = {
 
 // Create bot
 var bot = new builder.BotConnectorBot(botConnectorOptions);
-bot.add('/', function (session) {
-    
-    //respond with user's message
-    session.send("あなたは" + session.message.text + "と言いました。"
-     + "トップページへは https://mki365.sharepoint.com/sites/ba からアクセスできます。");
-});
+//bot.add('/', function (session) {
+//    
+//    //respond with user's message
+//    session.send("あなたは" + session.message.text + "と言いました。"
+//     + "トップページへは https://mki365.sharepoint.com/sites/ba からアクセスできます。");
+//});
+
+bot.add('/', new builder.CommandDialog()
+    .matches('^version', builder.DialogAction.send('PA Bot version 0.1')
+    .matches('google', builder.DialogAction.send('http://www.google.com/')
+    .matches('MKI', builder.DialogAction.send('http://www.mki.co.jp/')
+    .matches('プロダクトポータル', builder.DialogAction.send('https://mki365.sharepoint.com/sites/ba/')
+    .onDefault(builder.DialogAction.send("認識できません。"))
+);
 
 // Setup Restify Server
 var server = restify.createServer();
